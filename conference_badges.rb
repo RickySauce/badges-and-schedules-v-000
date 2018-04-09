2b28934121cc65c7dd37c889548181fe1765f231
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 15:12:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
 def batch_badge_creator(attendees)
-  attendees.all do |speakers|
+  attendees.all? do |speakers|
     return "Hello, my name is #{speakers}."
   end
 end
